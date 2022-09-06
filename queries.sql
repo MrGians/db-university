a247acd1176a517895bdbd3b0e6df47fcb192108
MrGians <gianlumura97@gmail.com>
2022-09-06 15:25:53 +0200

    ex-8 done | Query con Select All Done

--- queries.sql
+++ queries.sql
@@ -37,7 +37,9 @@ SELECT COUNT(*) AS `tot_departments`
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-
+SELECT COUNT(*) AS `no_phone_teachers` 
+FROM `teachers`
+WHERE `phone` IS NULL;
 
 
 -- ------------------------------------------ QUERY CON GROUP BY
