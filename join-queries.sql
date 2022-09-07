a6743b3458beae5bf5f1f84b2a81f3370bab1d4c
MrGians <gianlumura97@gmail.com>
2022-09-07 14:01:23 +0200

    ex-2 Done | JOIN

--- join-queries.sql
+++ join-queries.sql
@@ -7,7 +7,11 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-
+SELECT `degrees`.`name`, `departments`.`name`
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
